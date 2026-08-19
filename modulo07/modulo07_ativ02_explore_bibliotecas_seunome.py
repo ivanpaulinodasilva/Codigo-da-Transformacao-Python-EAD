11e09ab5f2170f239f23aed266b1549bcf90fd80
Aluno Vocacao <ivanemmobile@gmail.com>
2026-08-19 18:17:12 -0300

    reorganize

---
+++ modulo07/modulo07_ativ02_explore_bibliotecas_seunome.py
@@ -0,0 +1,20 @@
+'''
+Programador: as variaveis, serão inseridas no app - BACK-End
+
+Dev: existe a interação com o usuario - Web Design (Front-End)
+
+'''
+import utilidades
+
+num1 = 10
+num2 = 5
+
+print('⚙ 🧱Teste de Utilidades ⚙ 🧱')
+print(f'Números utilizados: {num1} e {num2}')
+
+
+print(f' Usando Adição ({num1} + {num2}) :', utilidades.soma(num1, num2))
+
+
+
+print(f' Usando Adição ({num1} - {num2}) :', utilidades.subtrair(num1, num2))
